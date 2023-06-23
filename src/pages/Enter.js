b26bc34231315437faca5551b70bbfc7091e858f
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-06-24 00:14:58 +0530

    Other Pages

--- src/pages/Enter.js
+++ src/pages/Enter.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import './css/Enter.css'
 import PreNavbar from '../components/PreNavbar'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 function Enter() {
+
+  const navigate = useNavigate()
+  useEffect (()=>{
+    navigate("/login")
+  }, [navigate])
+
   return (
     <div className='Enter'>
       <PreNavbar />
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './css/Error.css'
+import { Link } from 'react-router-dom'
 
 function Error() {
   return (
-    <div className='Error'>Error</div>
+    <div className='Error'>
+      <h1>404 Not Found</h1>
+      <Link to="/home">Go Back</Link>
+    </div>
   )
 }
 
